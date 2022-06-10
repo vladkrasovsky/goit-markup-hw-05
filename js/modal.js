20dbce2eef7a7e82e80069e2d2416c70af7dbb56
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-06-10 23:39:56 +0300

    Adds hidden overflow to body when modal is open

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,6 @@
 (() => {
   const refs = {
+    body: document.body,
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
@@ -10,5 +11,15 @@
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    refs.body.style.paddingRight = `${getBrwoserScrollbarWidth()}px`;
+    refs.body.style.overflow = 'hidden';
+    if (refs.modal.classList.contains('is-hidden')) {
+      refs.body.style.paddingRight = 0;
+      refs.body.style.overflow = 'auto';
+    }
+  }
+
+  function getBrwoserScrollbarWidth() {
+    return window.innerWidth - document.documentElement.clientWidth;
   }
 })();
