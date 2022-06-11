70c4401e19441f444dcf97645603b6dbf43a6214
Vlad Krasovsky <v.o.krasovsky@gmail.com>
2022-06-11 23:04:29 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,7 @@
 (() => {
   const refs = {
     body: document.body,
+    header: document.querySelector('.header'),
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
@@ -10,12 +11,15 @@
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
+    let scrollBarWidth = `${getBrwoserScrollbarWidth()}px`;
     refs.modal.classList.toggle('is-hidden');
-    refs.body.style.paddingRight = `${getBrwoserScrollbarWidth()}px`;
+    refs.body.style.paddingRight = scrollBarWidth;
     refs.body.style.overflow = 'hidden';
+    refs.header.style.right = scrollBarWidth;
     if (refs.modal.classList.contains('is-hidden')) {
       refs.body.style.paddingRight = 0;
       refs.body.style.overflow = 'auto';
+      refs.header.style.right = 0;
     }
   }
 
